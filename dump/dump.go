fb5ef5952f5dd41790a653090b8505a82a6534ba
Brett Vickers <brett@beevik.com>
2018-02-11 19:49:14 -0800

    dump util.

--- dump/dump.go
+++ dump/dump.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/beevik/hexdump"
 )
 
 func main() {
-	hexdump.Dump(make([]byte, 108), hexdump.FormatGo, os.Stdout)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Printf("Usage: dump [file]\n")
+		os.Exit(0)
+	}
+
+	b, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		return
+	}
+
+	hexdump.Dump(b, hexdump.FormatGo, os.Stdout)
 }
